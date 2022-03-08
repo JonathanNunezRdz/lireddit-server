4158419aa4a35d03ab5012df49a14c37ce6acc0b
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2022-03-07 19:12:53 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ const main = async () => {
 
 	// redisClient.connect()
 	// await redisClient.connect().catch(console.error);
-	app.set('proxy', 1);
+	app.set('trust proxy', 1);
 	app.use(
 		cors({
 			origin: process.env.CORS_ORIGIN,
